69d6e26d7af5c6428bff66f27580257587dd017c
prabhu k <prabhukuppan1110@gmail.com>
2023-07-26 17:53:34 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+
 
 function App() {
+  const [date,setDate]=useState(new Date());
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='text-center'>React Calendar</h1>
+      <div className='calender-container'>
+      <Calendar  onChange={setDate}value={date}/>
+    </div>
+    <div>
+    <p className='selectDate'>
+      <span className='bold'>Selected Date:</span>{''}
+      {date.toDateString()}
+
+    </p>
+    </div>
     </div>
   );
 }
